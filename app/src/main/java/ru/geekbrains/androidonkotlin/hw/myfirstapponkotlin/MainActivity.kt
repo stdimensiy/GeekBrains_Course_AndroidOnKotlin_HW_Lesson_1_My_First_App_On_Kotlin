07385f09ead3923b6dd5c5272a1e3e410a25ad34
StDimensiy <stdimensiy@yandex.ru>
2021-04-25 23:51:42 +0300

    ДЗ к уроку №1 пункт 1.4 - Кнопка добавлена ClickListener навешен! по клику на кнопке меняется текст в текстовом поле. Строки текста кнопки, начального текста текстового поля и текст результата размещены в ресурсах strings.xml

--- app/src/main/java/ru/geekbrains/androidonkotlin/hw/myfirstapponkotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/myfirstapponkotlin/MainActivity.kt
@@ -1,11 +1,19 @@
 package ru.geekbrains.androidonkotlin.hw.myfirstapponkotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // навешиваем кликлиссенер на первую кнопку
+        val button = findViewById<Button>(R.id.buttonOne);
+        val textView = findViewById<TextView>(R.id.resultClick);
+        button.setOnClickListener {
+            textView.text = resources.getString(R.string.result_message_button_press)
+        }
     }
 }
