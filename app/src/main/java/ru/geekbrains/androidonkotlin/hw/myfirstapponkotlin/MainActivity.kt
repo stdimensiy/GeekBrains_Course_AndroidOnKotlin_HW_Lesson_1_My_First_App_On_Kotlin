393535732c464d6f85478b58510eae6216ecf208
StDimensiy <stdimensiy@yandex.ru>
2021-04-25 21:04:00 +0300

    ДЗ к уроку №1 пункт 1.3 - MainActivity переведен на Kotlin! Конвертация выполнена в автоматическом режиме. Работоспособность проверена. все работает (пока).

--- app/src/main/java/ru/geekbrains/androidonkotlin/hw/myfirstapponkotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/androidonkotlin/hw/myfirstapponkotlin/MainActivity.kt
@@ -1,14 +1,11 @@
-package ru.geekbrains.androidonkotlin.hw.myfirstapponkotlin;
+package ru.geekbrains.androidonkotlin.hw.myfirstapponkotlin
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
